90d1020097f013a68ccd23f954e46ce500214d10
Jaco296c <jaco296c@outlook.dk>
2023-02-27 12:04:12 +0100

    L

--- H1_lomme/H1_lomme/Program.cs
+++ H1_lomme/H1_lomme/Program.cs
@@ -3,32 +3,37 @@ int Tal1 = Convert.ToInt32(Console.ReadLine());
 Console.WriteLine("Tal2:");
 int Tal2 = Convert.ToInt32(Console.ReadLine());
 
+
 Console.WriteLine(""" 
-    What do your whatsss
-    Addition
-    Subtraction
-    Multiplication
-    Division
-    kALsjdklsajd
-    aslkdjasd
+    What do your what
+    Addition - 1
+    Subtraction - 2
+    Multiplication - 3
+    Division - 4
     """);
-string Input = Console.ReadLine();
 
-if (Input.Contains("Add"))
+var Input = GetInput();
+
+switch (Input)
 {
-
+    case "1":
+        Console.WriteLine(Tal1+Tal2);
+        break;
+    case "2":
+        Console.WriteLine(Tal1 - Tal2);
+        break;
+    case "3":
+        Console.WriteLine(Tal1 * Tal2);
+        break;
+    case "4":
+        Console.WriteLine(Tal1 / Tal2);
+        break;
+    default:
+        Console.WriteLine("Invalid input");
+        break;
 }
-else if (Input.Contains("Sub"))
+
+string GetInput ()
 {
-
-}
-
-else if (Input == "Multi")
-{
-
-}
-
-else if (Input == "Div")
-{
-
+    return Console.ReadLine();
 }
